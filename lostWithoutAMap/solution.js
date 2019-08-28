89cdf354c3e22d815c0ec7d12d8febce14a849a5
Jordan Dillard <jordan.dillard13@gmail.com>
2019-08-28 08:58:18 -0500

    added prompt and solution for lostWithoutAMap

---
+++ lostWithoutAMap/solution.js
@@ -0,0 +1,9 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] will return [2, 4, 6]
+
+function lostWithoutAMap(array) {
+  return array.map(i => i * 2);
+}
