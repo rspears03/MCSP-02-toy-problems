ce27927ebb962f3fca9df2ce4792a5b98bacaf37
Rodney Spears <rodneyspears@gmail.com>
2019-12-19 09:59:12 -0600

    maxConsecutiveOnes

--- maxConsecutiveOnes/prompt.js
+++ maxConsecutiveOnes/prompt.js
@@ -3,5 +3,36 @@
 //maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3]) returns 4
 
 function maxConsecutiveOnes(array) {
-  //write solution code
+  // 1) Understand the problem :
+//  Write a function that takes in an array of numbers and returns the maximum number of consecutive 1s
+
+// I: array
+// O: return a number that display the maximum number of consecutive 1s
+// C:
+// E:
+
+  // make a val that will hold the amount of consecutive 1s
+    let count = 0
+    let finCount = 0
+  // loop into the array
+    for(let i = 0; i < array.length; i++){
+        // make a val count
+         
+        // if the index of the array equal one
+          if(array[i] === 1){
+                // the count goes up by one
+                count++
+//                 finCount = count
+          }else{
+              if(count > finCount){
+                  
+              }
+          }
+           // return finCount
+              finCount = count
+          console.log(count)
+    }
 }
+ 
+
+maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3])
--- militaryTime/prompt.js
+++ militaryTime/prompt.js
@@ -4,3 +4,17 @@
 function militaryTime(timeStr) {
   //write code here
 }
+
+// 1) Understand the problem :
+// Write a function that takes in a string of the time and returns the time in military format
+// I: timeStr(A string that is a number in 12 hours time)
+// O: A new string milTime(return to the user a string that is convert from 12hour time to 24hour time)
+// C:
+// E:
+
+// 2) Devise a Strategy
+//  1)Plan 1
+  //take timeStr and run the includes method to check if timeStr includes a "pm" or "am"
+  // if the time includes a "pm" split the string at : 
+  // add 12 to the first number 
+  // catcot the array but together
