e9e7e429d359d1f219ef21b29daee28b1cd873bd
Rodney Spears <rodneyspears@Rodneys-MacBook-Pro.local>
2020-02-20 18:35:19 -0600

    finished binarySearch toy-problem

--- binarySearchArray/prompt.js
+++ binarySearchArray/prompt.js
@@ -10,4 +10,21 @@
  * console.log(index); // null
  */
 
-var binarySearch = function(array, target) {};
+var binarySearch = function(array, target) {
+    if(!array.includes(target)){
+        return null
+    }else{
+        for(var i = 0; i < array.length; i++){
+            if(array.indexOf(target) === array[i]){
+                return array[i];//?
+            }
+        }
+    }
+};
+
+var index = binarySearch([1, 2, 3, 4, 5], 4);
+console.log(index); // 3
+
+// var index = binarySearch([1, 2, 3, 4, 5], 8);
+// console.log(index); // null
+
