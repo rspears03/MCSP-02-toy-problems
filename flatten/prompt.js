484e49ec72d20c228c92906d47c647162c3dc7eb
Rodney Spears <rodneyspears@Rodneys-MacBook-Pro.local>
2020-02-24 23:36:25 -0600

    toy-problems flatten  finished

--- flatten/prompt.js
+++ flatten/prompt.js
@@ -3,4 +3,20 @@
 // Example:
 // flatten([1,['a', 2, [8, true], 7]]) returns [1, 'a', 2, 8, true, 7]
 
-function flatten(array) {}
+// const flatten = function(array) {
+//    return array.flat(Infinity)
+// };
+
+
+const flatten = function(arr, result = []) {
+    for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            flatten(arr[i], result)
+        }else{
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+flatten([1,['a', 2, [8, true], 7]])//? 
